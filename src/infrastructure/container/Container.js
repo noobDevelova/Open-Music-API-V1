87fbfec4d5f58f7b6e0a57301d8d943ee0e07e43
Randi <randyrisdiansyah@gmail.com>
2025-03-24 23:50:44 +0700

    feat(infrastructure/container): register create, edit, delete, get album use case

--- src/infrastructure/container/Container.js
+++ src/infrastructure/container/Container.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 const AlbumRepository = require('../../domains/albums/repository/AlbumsRepository');
 const GetListAlbumsUseCase = require('../../domains/albums/use-cases/GetListAlbumsUseCase');
+const GetAlbumUseCase = require('../../domains/albums/use-cases/GetAlbumUseCase');
+const DeleteAlbumUseCase = require('../../domains/albums/use-cases/DeleteAlbumUseCase');
+const EditAlbumUseCase = require('../../domains/albums/use-cases/EditAlbumUseCase');
+const CreateAlbumUseCase = require('../../domains/albums/use-cases/CreateAlbumUseCase');
 
 class Container {
   constructor() {
@@ -26,6 +30,30 @@ class Container {
     this.register(
       'getListAlbumsUseCase',
       () => new GetListAlbumsUseCase(this.get('albumRepository')),
+      true
+    );
+
+    this.register(
+      'getAlbumUseCase',
+      () => new GetAlbumUseCase(this.get('albumRepository')),
+      true
+    );
+
+    this.register(
+      'deleteAlbumUseCase',
+      () => new DeleteAlbumUseCase(this.get('albumRepository')),
+      true
+    );
+
+    this.register(
+      'editAlbumUseCase',
+      () => new EditAlbumUseCase(this.get('albumRepository')),
+      true
+    );
+
+    this.register(
+      'createAlbumUseCase',
+      () => new CreateAlbumUseCase(this.get('albumRepository')),
       true
     );
   };
