8d9e60a99991fb0481e3820cedc4acca9fee8070
Randi <randyrisdiansyah@gmail.com>
2025-03-24 23:47:56 +0700

    feat(commons): add constants for album response body

--- src/commons/constants/index.js
+++ src/commons/constants/index.js
@@ -1,15 +1,27 @@
 const ALBUM_NOT_FOUND = 'Album Not Found.';
 const ALBUM_NOT_MEET_DATA_SPECIFICATION = 'Album Not Meet Data Specification.';
+const ALBUM_UPDATED = 'Album Updated.';
+const ALBUM_DELETED = 'Album Deleted.';
+const ALBUM_CREATED = 'Album Created.';
 
 const SONG_NOT_FOUND = 'Song Not Found.';
 const SONG_NOT_MEET_DATA_SPECIFICATION = 'Song Not Meet Data Specification.';
 
 const INTERNAL_SERVER_ERROR = 'Internal Server Error.';
+const FAIL_TO_DELETE = 'Fail to delete resource.';
+const FAIL_TO_UPDATE = 'Fail to update resource.';
 
 module.exports = {
   ALBUM_NOT_FOUND,
   ALBUM_NOT_MEET_DATA_SPECIFICATION,
+  ALBUM_UPDATED,
+  ALBUM_DELETED,
+  ALBUM_CREATED,
+
   SONG_NOT_FOUND,
   SONG_NOT_MEET_DATA_SPECIFICATION,
+
   INTERNAL_SERVER_ERROR,
+  FAIL_TO_DELETE,
+  FAIL_TO_UPDATE,
 };
