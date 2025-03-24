61f80d5bf9e53d107ef63c669303fe44ff668978
Randi <randyrisdiansyah@gmail.com>
2025-03-24 23:51:16 +0700

    feat(http/api/albums): add domains implementations to handler

--- src/interfaces/http/api/albums/handler.js
+++ src/interfaces/http/api/albums/handler.js
@@ -1,16 +1,83 @@
 const ContainerProvider = require('../../../../infrastructure/container');
-const { ResponseBuilder } = require('../../../../commons');
+const {
+  ResponseBuilder,
+  ALBUM_NOT_FOUND,
+  ALBUM_UPDATED,
+  ALBUM_DELETED,
+  ALBUM_CREATED,
+  FAIL_TO_DELETE,
+  FAIL_TO_UPDATE,
+} = require('../../../../commons');
 
 class AlbumsHandler {
   constructor() {
     const container = ContainerProvider.getInstance();
+
     this._getListAlbumsUseCase = container.get('getListAlbumsUseCase');
+    this._getAlbumUseCase = container.get('getAlbumUseCase');
+    this._deleteAlbumUseCase = container.get('deleteAlbumUseCase');
+    this._editAlbumUseCase = container.get('editAlbumUseCase');
+    this._createAlbumUseCase = container.get('createAlbumUseCase');
   }
 
   getListAlbumsHandler = async (request, h) => {
     const albums = await this._getListAlbumsUseCase.execute();
 
     return ResponseBuilder.success(h, { data: { albums } });
+  };
+
+  getAlbumHandler = async (request, h) => {
+    const { id } = request.params;
+    const album = await this._getAlbumUseCase.execute(id);
+
+    if (!album) {
+      return ResponseBuilder.notFound(h, { message: ALBUM_NOT_FOUND });
+    }
+
+    return ResponseBuilder.success(h, { data: { album } });
+  };
+
+  deleteAlbumHandler = async (request, h) => {
+    const { id } = request.params;
+    const album = await this._getAlbumUseCase.execute(id);
+
+    if (!album) {
+      return ResponseBuilder.notFound(h, {
+        message: `${FAIL_TO_DELETE} ${ALBUM_NOT_FOUND}`,
+      });
+    }
+
+    await this._deleteAlbumUseCase.execute(id);
+
+    return ResponseBuilder.success(h, {
+      message: ALBUM_DELETED,
+    });
+  };
+
+  editAlbumHandler = async (request, h) => {
+    const { id } = request.params;
+    const album = await this._getAlbumUseCase.execute(id);
+
+    if (!album) {
+      return ResponseBuilder.notFound(h, {
+        message: `${FAIL_TO_UPDATE} ${ALBUM_NOT_FOUND}`,
+      });
+    }
+
+    await this._editAlbumUseCase.execute(id, request.payload);
+
+    return ResponseBuilder.success(h, {
+      message: ALBUM_UPDATED,
+    });
+  };
+
+  createAlbumHandler = async (request, h) => {
+    const albumId = await this._createAlbumUseCase.execute(request.payload);
+
+    return ResponseBuilder.created(h, {
+      data: { albumId },
+      message: ALBUM_CREATED,
+    });
   };
 }
 
--- src/interfaces/http/api/albums/routes.js
+++ src/interfaces/http/api/albums/routes.js
@@ -4,6 +4,26 @@ const routes = handler => [
     path: '/albums',
     handler: handler.getListAlbumsHandler,
   },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: handler.getAlbumHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: handler.editAlbumHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: handler.deleteAlbumHandler,
+  },
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: handler.createAlbumHandler,
+  },
 ];
 
 module.exports = routes;
